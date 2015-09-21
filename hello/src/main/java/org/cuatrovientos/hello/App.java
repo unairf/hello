3b02b10633bcf711c653aa25ec5d98552a392758
Unai Rodríguez <unairf@gmail.com>
2015-09-21 09:32:18 +0200

    First commit

---
+++ hello/src/main/java/org/cuatrovientos/hello/App.java
@@ -0,0 +1,16 @@
+package org.cuatrovientos.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        hello();
+    }
+    public static void hello() {
+    	System.out.println("Hello DAM");
+    }
+}
